0657486de40a28633b30969bb45910144a4eb764
igdev <dorultanianos@gmail.com>
2025-04-20 12:49:11 +0300

    feat: implement EIP6963 types

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app'
 import './index.css'
+import {EIP6963AnnounceProviderEvent} from '../utils/types';
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface CustomEventMap {
+  "eip6963:announceProvider": CustomEvent<EIP6963AnnounceProviderEvent>;
+}
+
+declare global {
+  interface Document {
+    addEventListener<K extends keyof CustomEventMap>(
+        type: K,
+        listener: (this: Document, ev: CustomEventMap[K]) => void
+    ): void;
+    dispatchEvent<K extends keyof CustomEventMap>(ev: CustomEventMap[K]): void;
+  }
+  interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    ethereum: any;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,3 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_ALCHEMY_KEY: string;
 }
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
